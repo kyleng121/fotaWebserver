65a067ef854579ce9a757c123c851447fa347b05
kyleng121 <96457004+kyleng121@users.noreply.github.com>
2023-07-30 16:38:16 +0700

    Update settings.py

--- fotaWebserver/settings.py
+++ fotaWebserver/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-CSRF_TRUSTED_ORIGINS=['http://fotaserver.azurewebsites.net/*']
+CSRF_TRUSTED_ORIGINS=['*']
 
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
