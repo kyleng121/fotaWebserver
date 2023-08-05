6063a799328d19185b4af977a28fe5f4c6056655
kyleng121 <96457004+kyleng121@users.noreply.github.com>
2023-08-05 11:16:57 +0700

    Create azure_messages_hndlr.py

---
+++ interfacesApi/azure_messages_hndlr.py
@@ -0,0 +1,28 @@
+import json
+import pandas as pd
+from azure.iot.device import IoTHubDeviceClient
+import sys
+from azure.iot.hub import IoTHubRegistryManager
+
+CONNECTION_STRING = "HostName=FotaWebserver.azure-devices.net;SharedAccessKeyName=service;SharedAccessKey=WEfT/T/BkiYCv1DiOPT0zm8vbuAHdIX/Ppo7zUd1V4E="
+DEVICE_ID = "raspberry4"
+
+def send_request_messagses(data,messageType):
+    try:
+        # Create IoTHubRegistryManager
+        registry_manager = IoTHubRegistryManager(CONNECTION_STRING)
+
+        props={}
+        # optional: assign system properties
+        props.update(messageId = messageType)
+
+        props.update(contentType = "application/json")
+
+        registry_manager.send_c2d_message(DEVICE_ID, data, properties=props)
+
+    except Exception as ex:
+        print ( "Unexpected error" )
+        return
+
+
+
