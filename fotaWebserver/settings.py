27e3929e4a87a4667d8ca87031b103761003d92d
kyleng121 <96457004+kyleng121@users.noreply.github.com>
2023-08-05 12:06:01 +0700

    Update settings.py

--- fotaWebserver/settings.py
+++ fotaWebserver/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.CrossOriginOpenerPolicyMiddleware',
 ]
 
 ROOT_URLCONF = 'fotaWebserver.urls'
