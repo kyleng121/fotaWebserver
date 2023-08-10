7df6d60c2e99210b161974b7d70964b0b83a8543
kyleng121 <96457004+kyleng121@users.noreply.github.com>
2023-08-10 16:45:56 +0700

    Update azure_file_controller.py

--- interfacesApi/azure_file_controller.py
+++ interfacesApi/azure_file_controller.py
@@ -20,42 +20,46 @@ container_name = "fwupload"
 def upload_to_blob_storage(file,file_name):
     print("in upload function to blob")
     blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+    print("in upload function to blob1")
     blob_client = blob_service_client.get_blob_client(container=container_name,blob=file_name)
+    print("in upload function to blob2")
     blob_client.upload_blob(data=file)
+    print("in upload function to blob3")
     file_object = save_file_url_to_db(blob_client.url)
+    print("in upload function to blob4")
     return file_object
 
-def create_blob_client(file_name):
+# def create_blob_client(file_name):
 
-    account_url = 'https://fotafwstorage.blob.core.windows.net'
-    # container_name = container_name
-    default_credential = DefaultAzureCredential()
-    blob_service_client = BlobServiceClient(account_url, credential=default_credential)
-    container_client = blob_service_client.create_container(container_name)
-    # secret_client = SecretClient(
-    #     vault_url=settings.AZURE_VAULT_ACCOUNT, credential=default_credential
-    # )
+#     account_url = 'https://fotafwstorage.blob.core.windows.net'
+#     # container_name = container_name
+#     default_credential = DefaultAzureCredential()
+#     blob_service_client = BlobServiceClient(account_url, credential=default_credential)
+#     container_client = blob_service_client.create_container(container_name)
+#     # secret_client = SecretClient(
+#     #     vault_url=settings.AZURE_VAULT_ACCOUNT, credential=default_credential
+#     # )
 
-    # storage_credentials = secret_client.get_secret(name=settings.AZURE_STORAGE_KEY_NAME)
+#     # storage_credentials = secret_client.get_secret(name=settings.AZURE_STORAGE_KEY_NAME)
 
-    return BlobClient(
-        account_url,
-        container_name,
-        blob_name=file_name,
-        # credential=storage_credentials.value,
-    )
+#     return BlobClient(
+#         account_url,
+#         container_name,
+#         blob_name=file_name,
+#         # credential=storage_credentials.value,
+#     )
 
 def check_file_ext(path):
     ext = Path(path).suffix
     return ext in ALLOWED_EXTENTIONS
 
 
-def download_blob(file):
-    blob_client = create_blob_client(file)
-    if not blob_client.exists():
-        return
-    blob_content = blob_client.download_blob()
-    return blob_content
+# def download_blob(file):
+#     blob_client = create_blob_client(file)
+#     if not blob_client.exists():
+#         return
+#     blob_content = blob_client.download_blob()
+#     return blob_content
     
 
 def save_file_url_to_db(file_url):
