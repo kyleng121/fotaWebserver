90112c3ffdce988dec96157adc880405f793ae50
kyleng121 <96457004+kyleng121@users.noreply.github.com>
2023-08-11 16:23:08 +0700

    Update urls.py

--- interfacesApi/urls.py
+++ interfacesApi/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('upload_file', views.upload_file, name="upload_file"),
     path('list_files/', views.list_files, name="list_files"),
-    path('download_file/<int:file_id>/', views.download_file, name="download_file"),
-    path('delete_file/<int:file_id>/', views.delete_file, name="delete_file"),
+    # path('download_file/<int:file_id>/', views.download_file, name="download_file"),
+    # path('delete_file/<int:file_id>/', views.delete_file, name="delete_file"),
     path('get_diag_messages',views.get_diag_messages,name='get_diag_messages'),
 ]
