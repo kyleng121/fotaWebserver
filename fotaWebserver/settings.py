6197b070944ebb67825bddfdc6466797c3f56b7f
kyleng121 <96457004+kyleng121@users.noreply.github.com>
2023-08-13 15:05:34 +0700

    Update settings.py

--- fotaWebserver/settings.py
+++ fotaWebserver/settings.py
@@ -80,7 +80,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'fotaWebserver.wsgi.application'
-
+ASGI_APPLICATION = 'fotaWebserver.routing.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
