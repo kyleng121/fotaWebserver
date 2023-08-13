2befa8a2a51aef20787f253b55c22a0589b9df72
kyleng121 <96457004+kyleng121@users.noreply.github.com>
2023-08-13 15:31:42 +0700

    Add files via upload

---
+++ interfacesApi/consumers.py
@@ -0,0 +1,18 @@
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+class DataConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        pass
+
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        value = data.get('value')  # Adjust this based on your data structure
+
+        # Send the received data to all connected clients
+        await self.send(text_data=json.dumps({
+            'value': value
+        }))
